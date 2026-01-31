8683b9ebd206730c2cf935bd0e467c25377fce79
Ashref Ben Amor <ashref.benamor@esprit.tn>
2026-01-31 23:49:34 +0100

    Initial commit

---
+++ src/main/java/tn/esprit/ashref_ben_amor_4ds9/AshrefBenAmor4Ds9Application.java
@@ -0,0 +1,13 @@
+package tn.esprit.ashref_ben_amor_4ds9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshrefBenAmor4Ds9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AshrefBenAmor4Ds9Application.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/ashref_ben_amor_4ds9/AshrefBenAmor4Ds9ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.ashref_ben_amor_4ds9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshrefBenAmor4Ds9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
